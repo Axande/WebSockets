00a678ee9082ca2bae5c9d925a334bb8f2f06b3d
Andrei <Axande@users.noreply.github.com>
2020-03-03 09:34:09 +0100

    Add project files.

---
+++ signalR/Hubs/CommandsHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebSockets.signalR.Hubs
+{
+    public class CommandsHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        { 
+            Console.WriteLine("SendMessage");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
